66dd88bbc9d270f2cbdd1ac228516ff7f8079fab
Dan Bennett <@nonodename.com>
2025-08-10 17:17:20 -0500

    Fixed formatting

--- src/parse_triple.hpp
+++ src/parse_triple.hpp
@@ -1,11 +1,5 @@
 #pragma once
 #include <string>
 
-bool ParseTripleLine(
-    const std::string &line,
-    std::string &subject,
-    std::string &predicate,
-    std::string &object,
-    std::string &lang_tag,
-    std::string &datatype_iri
-);
+bool ParseTripleLine(const std::string &line, std::string &subject, std::string &predicate, std::string &object,
+                     std::string &lang_tag, std::string &datatype_iri);
